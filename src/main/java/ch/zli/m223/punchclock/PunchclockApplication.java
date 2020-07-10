265fb0a9885e1aacc8db4c6b3a9a700d17f1b04e
612Noah612 <noah.halvorsrod@gmail.com>
2020-07-10 09:00:15 +0200

    Commit

--- src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
+++ src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
@@ -2,12 +2,20 @@ package ch.zli.m223.punchclock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PunchclockApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(PunchclockApplication.class, args);
+	}
+	//code from: https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
